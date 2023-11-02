c980ca6b4ed164b099031686dbd53c4d7cf13446
iflamed <iflamed@gmail.com>
2023-11-02 23:15:25 +0800

    fix find user bug

--- db/user.go
+++ db/user.go
@@ -13,19 +13,19 @@ type User struct {
 }
 
 func (u *User) Save(ctx context.Context) error {
-	return db.WithContext(ctx).Save(u).Error
+	return DB().WithContext(ctx).Save(u).Error
 }
 
 func GetUser(ctx context.Context, uid uint) *User {
 	u := &User{}
-	db.WithContext(ctx).Where("id", uid).First(u)
+	DB().WithContext(ctx).Where("id", uid).First(u)
 	return u
 }
 
 func RegisterUser(ctx context.Context, accountID uint) (*User, error) {
 	u := &User{}
 	err := DB().WithContext(ctx).Where("account_id = ?", accountID).First(u).Error
-	if !errors.Is(err, gorm.ErrRecordNotFound) {
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, err
 	}
 	u.AccountID = accountID
